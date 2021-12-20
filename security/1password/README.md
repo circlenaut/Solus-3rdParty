327ed2365fd7f4dde6152c1cb85dd8005e78ebe9
Phillip Strefling <phillip@strefling.pro>
2021-12-19 19:05:16 -0500

    updated 1Password

--- security/1password/README.md
+++ security/1password/README.md
@@ -1,6 +1,4 @@
 # Release source (where can I find versioned releases?):
-https://downloads.1password.com/linux/debian/amd64/beta/1password-latest.deb
-
 https://releases.1password.com/linux/
 https://snapcraft.io/1password
 https://support.1password.com/betas/#install-a-beta-release-of-1password-for-linux
--- security/1password/actions.py
+++ security/1password/actions.py
@@ -10,12 +10,13 @@ IgnoreAutodep = True
 # Should not change.
 Suffix = "-1"
 
+target_version = "1password-%s.x64" % get.srcVERSION()
+
 def setup():
     shelltools.system("pwd")
-    shelltools.system("ar xf 1password-latest.deb")
-#    shelltools.system("ar xf 1password-%s_amd64.deb" % (get.srcVERSION()))
-    shelltools.system("tar xvf data.tar.xz")
+    shelltools.system("tar xvf 1password-%s.x64.tar.gz" % get.srcVERSION())
 
 def install():
-    pisitools.insinto("/", "opt")
-    pisitools.insinto("/", "usr")
+    pisitools.insinto("/opt/1Password/", "1password-%s.x64/*" % get.srcVERSION() )
+    pisitools.insinto("/usr/share/applications/", "1password-%s.x64/resources/1password.desktop" % get.srcVERSION() )
+    pisitools.insinto("/usr/share/icons/hicolor/", "1password-%s.x64/resources/icons/hicolor/*" % get.srcVERSION() )
