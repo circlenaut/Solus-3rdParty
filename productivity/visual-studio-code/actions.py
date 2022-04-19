ed115a284b88039834c6a52851ab0b4809bf012d
Phillip Strefling <phillip@strefling.pro>
2022-04-19 11:47:49 -0400

    updated vscode

--- productivity/visual-studio-code/actions.py
+++ productivity/visual-studio-code/actions.py
@@ -45,7 +45,7 @@ IgnoreAutodep = True
 Suffix = "-1"
 
 # Change with each release
-SUBVERSION = 1646927742
+SUBVERSION = 1649664567
 
 def test_cmd(cmd):
     xml_path = _os.path.join(cmd, 'metadata.xml')
